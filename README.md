7b726e367bdd8b3becb0d6fa7025700d9be7ca61
Peeyush Dyavarashetty <32363748+Peeyush4@users.noreply.github.com>
2024-09-02 22:29:26 -0400

    All code and input videos

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Real-time Image segmentation for autonomous driving
-Real-time Image Segmentation for Autonomous Driving. 
+## Each folder/file information
+
+files/: This is the place where each model information along with .pth file and proof that we used models like PPLite B50, T50, STDC and DDRNet and all models' plots.
+models/: This contains models Fast-SCNN and BiSeNet
+results/: This is the place where model results are dumped
+videos/: This is the place where videos are placed
+
+config_v2.0.json: Configuration used by Mapillary
+bisenet_video.py: File required to run BiSeNet model on videos
+fast_scnn_video.py: File required to run Fast-SCNN model on videos
+MSML612 Presentation.pptx: Our presentation
+MSML612 Report: Report containing all the information
+Presentation_video.mp4: Our presentation video explaining the presentation
+requirements.txt: Contains which libraries to download
+
+
+## Instructions 
+Main python codes to run are bisenet_video.py and fast_scnn.py
+
+You can run the codes for BiSeNet by running the following in the terminal
+python bisenet_video.py -f <file_name> -d <directory> -m <model_name>
+
+You can run the codes for Fast-SCNN by running the following in the terminal
+python fast_scnn_video.py -f <file_name> -d <directory> -m <model_name>
+
+Where the <file_name>, <directory> and <model_name> are as follows:
+<file_name> is the file name from the videos/ folder,
+
+<directory> is the name of the folder from the folder "files/" as this indicates the model to use. The following folders only
+- bisenet_crossEntropyLoss_Weights
+- bisenet_focalLoss
+- bisenet_poly_lr_research_paper
+- scnn_crossEntropyLoss
+- scnn_crossEntropyLoss_Weights
+- scnn_focalLoss_3gamma
+- scnn_sgd_optimizer_poly
+
+<model_name> is the .pth file inside the those respective directories in the <directory> folder. You can choose which .pth file to choose in the <directory> folder.
+
+BiSeNet codes run only when bisenet directories are given to bisenet_video.py and Fast-SCNN run only when fast_scnn_video.py
+
+Now, the example code is 
+python bisenet_video.py -f transcodedVideo.mp4 -m bisenet_crossEntropyLoss_Weights_1e-4lr_60epochs_after_1e-lr_30_epochs -d bisenet_crossEntropyLoss_Weights
+
+When the code runs, the result will be in the results folder with the name <file_name>_<model_name>.mp4 and the output will be similar to the below lines 
+Number of frames: 601
+Reading time: 0.29398131370544434
+Transform time: 21.074201822280884
+Model time: 2.932260751724243
+Writing time: 40.23506021499634
+Total time: 64.53550410270691
+
---
+++ fast_scnn_video.py
@@ -0,0 +1,149 @@
+import cv2
+import torch
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+import time
+import os
+import matplotlib.pyplot as plt
+import json
+
+from models.fast_scnn import FastSCNN
+import argparse
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+parser = argparse.ArgumentParser(
+                    # prog='FastSCNN Semantic Segmentation',
+                    description='Takes video and creates semantic segmentation',
+                    epilog='Text at the bottom of help')
+parser.add_argument('-m', '--model_name')           # positional argument
+parser.add_argument('-f', '--filename')           # positional argument
+parser.add_argument('-d', '--directory')
+args = parser.parse_args()
+
+folder_name = args.directory#"scnn_crossEntropyLoss_Weights"
+model_name = args.model_name#"scnn_crossEntropyLoss_Weights_1e-5lr_60epochs_after_1e-3lr_30epochs"
+
+assert model_name, "Model name is required"
+assert folder_name, "Directory is required"
+assert args.filename, "Filename is required"
+
+if model_name.endswith('.pth'):
+    model_name = model_name[:-4]  # Remove the file extension
+    
+base_path = "."
+video_path = f"{base_path}/videos/{args.filename}"
+model_path = f"{base_path}/files/{folder_name}"
+weights_path = f"{model_path}/{model_name}.pth"
+
+config_path = f"{base_path}/config_v2.0.json"
+results_path = f"{base_path}/results"
+output_path = f"{results_path}/{args.filename}_{model_name}.mp4"
+
+assert os.path.exists(video_path), f"Video file not found at {video_path}"
+assert os.path.exists(model_path), f"Model folder not found at {model_path}"
+assert os.path.exists(weights_path), f"Model weights not found at {weights_path}"
+
+
+#Config map
+config = json.load(open(config_path))
+config_map = {label: config['labels'][label]['color'] 
+              for label in range(len(config['labels']))}
+
+#Applying config map
+# def apply_config_map(segmentation, config_map, device=device):
+#     # Create a color map array
+#     colormap = torch.tensor([config_map[i] for i in range(len(config_map))], dtype=torch.uint8).to(device)
+    
+#     # Use the segmentation array as indices to map colors
+#     colored_segmentation = colormap[segmentation]
+    
+#     return colored_segmentation.to('cpu').numpy()   
+
+# Define transformation (resize, normalize, etc.) if necessary
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((1024, 2048)),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    # Add other necessary transforms like normalization here
+])
+
+# Load pre-trained model 
+model = FastSCNN(num_classes=124)
+model = model.to(device)
+model.load_state_dict(torch.load(weights_path, weights_only=True))
+model.eval()
+
+# Load the video
+# video_path = 'input_video.mp4'
+cap = cv2.VideoCapture(video_path)
+fps = cv2.CAP_PROP_FPS
+# Define the codec and create a VideoWriter object to save the output video
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(output_path, fourcc, cap.get(cv2.CAP_PROP_FPS),
+                        (2048, 1024))
+                    #   (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+frames = 0
+overall_reading_time = 0
+overall_transform_time = 0
+overall_model_time = 0
+overall_writing_time = 0
+
+torch.cuda.empty_cache()
+colormap = torch.tensor([config_map[i] for i in range(len(config_map))], dtype=torch.uint8).to(device)
+
+while cap.isOpened():
+    # print(i, end=" ")
+    start = time.time()
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frames += 1
+    reading_time = time.time()
+    # Convert the frame to PIL image
+    pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    
+    # Apply the necessary transformations
+    input_tensor = transform(pil_image).unsqueeze(0)  # Add batch dimension
+    
+    # Move the input tensor to the appropriate device (GPU/CPU)
+    input_tensor = input_tensor.to(device)
+    transform_time = time.time()
+    
+    # Perform segmentation
+    with torch.no_grad():
+        output = model(input_tensor)
+    model_time = time.time()
+    
+    # Process the output to get the segmentation mask
+    # Assuming the output is a logit tensor, apply softmax or argmax as needed
+    segmentation = output[0].argmax(dim=1).squeeze().detach()
+    
+    # Optionally, map class indices to colors
+    segmentation = colormap[segmentation].to('cpu').numpy()  # Define this function as needed
+
+    # Convert segmentation mask to a format suitable for video saving
+    # segmentation = np.uint8(segmentation * 255)  # Example: binary mask
+    # segmentation = cv2.cvtColor(segmentation, cv2.COLOR_GRAY2BGR)
+    
+    # Write the frame to the output video
+    out.write(segmentation)
+    writing_time = time.time()
+
+    overall_reading_time += reading_time - start
+    overall_transform_time += transform_time - reading_time
+    overall_model_time += model_time - transform_time
+    overall_writing_time += writing_time - model_time 
+# Release everything when done
+cap.release()
+out.release()
+cv2.destroyAllWindows() 
+
+#Printout
+print(f"Number of frames: {frames}")
+print(f"Reading time: {overall_reading_time}")
+print(f"Transform time: {overall_transform_time}")
+print(f"Model time: {overall_model_time}")
+print(f"Writing time: {overall_writing_time}")
+print(f"Total time: {overall_reading_time + overall_transform_time + overall_model_time + overall_writing_time}")
